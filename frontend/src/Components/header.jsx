be14fa9a44d7cd2cd1681b126524b97c0ba01b1e
Amélie Grenier <amelie.grenier@hotmail.fr>
2023-03-01 10:31:14 +0100

    add list header

--- frontend/src/Components/header.jsx
+++ frontend/src/Components/header.jsx
@@ -3,6 +3,7 @@ import { useSignOut } from 'react-auth-kit'
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import bars from '../Assets/bars-solid.svg';
+import { useState } from 'react';
 
 
 function Header() {
@@ -14,6 +15,8 @@ function Header() {
         signOut();
         navigate("/login")
     }
+
+    const [open, setIsOpen] = useState(false);
 
     if (window.location.pathname === '/signin' ) {
         return null
@@ -31,9 +34,18 @@ function Header() {
                     <button className='button-account redirect'><Link to='/'>Accueil</Link></button>
                     <button className='button-account redirect'><Link to='/about'>A Propos</Link></button>
                     {/* <Link to='/login'><button className="log-btn" onClick={logout}>Se déconnecter</button></Link> */}
-                    <button className='button-account profil'>
+                    <button className={'button-account profil ' + (open ? 'open' : 'close')} onClick={() => {setIsOpen(!open)}} >
                             <img className='icon-bars' src={bars} alt='icon'/>
                             <img className='profil-picture-img' src='https://static3.depositphotos.com/1000951/138/i/450/depositphotos_1380772-stock-photo-profile-of-beautiful-smiling-girl.jpg' alt='profil'/>
+                            <div className='profile-menu'>
+                                <ul className='header-list'>
+                                    <Link to='/favorite'><button className='btn-list'>Favoris</button></Link>
+                                    <Link to='/hostings'><button className='btn-list'>Gérer mes annonces</button></Link>
+                                    <Link to='/new-hosting'><button className='btn-list'>Créer une annonce</button></Link>
+                                    <Link to='/account-settings'><button className='btn-list'>Compte</button></Link>
+                                    <Link to='/login'><button className='btn-list' onClick={logout}>Deconnexion</button></Link>
+                                </ul>
+                            </div>
                     </button>
                 </nav>
             </div>
